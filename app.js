c7924498093cf545ffcc470f66c80fd511ee07bd
Raj Upadhyay <rupadhyay523@gmail.com>
2022-09-10 17:26:55 +0530

    database connected

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-require('dotenv').config()
+require("dotenv").config();
 
-import express from 'express';
-import errorHandler from 'errorhandler';
-import { json, urlencoded } from 'body-parser';
-import methodOverride from 'method-override';
-import logger from 'morgan';
+const express = require("express");
+const errorHandler = require("errorhandler");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const logger = require("morgan");
+const { pool } = require("./config/db/index");
 
-const app = express()
-const port = 3000 || process.env.PORT
+const app = express();
+const port = 3000 || process.env.PORT;
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
-app.use(errorHandler())
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(errorHandler());
 
-app.use((req, res, next) => {
-})
+// app.use((req, res, next) => {
 
-app.get('/', async (req, res) => {
+// });
 
-  })
+app.get("/", (req, res) => {
+  res.status(200).json("hey");
+});
 
-app.get('/*/:uid', async (req, res) => {
-  
-})
+app.get("/db", (req, res) => {
+  pool.query('SELECT * FROM public."User"', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+// app.get("/*/:uid", async (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});

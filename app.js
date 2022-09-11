d24297fb1dc23c624a6988d72b42705e4e20298a
Raj Upadhyay <rupadhyay523@gmail.com>
2022-09-11 10:42:20 +0530

    food schema added with items,order and store

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const { pool } = require("./db/config/index");
 
 // car pool
 const carPool = require("./db/car_pooling");
+// food schema
+const food = require("./db/food");
 
 const app = express();
 const port = 3000 || process.env.PORT;
@@ -43,7 +45,25 @@ app.post("/car_pooling", carPool.createCarPool);
 app.put("/car_pooling/:id", carPool.updateCarPool);
 app.delete("/car_pooling/:id", carPool.deleteCarPool);
 
-// cleaning
+// food routes
+// food.items
+app.get("/food/items/:id", food.items.getItemById);
+app.post("/food/items", food.items.createItem);
+app.put("/food/items/:id", food.items.updateItem);
+app.delete("/food/items/:id", food.items.deleteItem);
+
+//food.order
+
+app.get("/food/order/:id", food.order.getOrderById);
+app.post("/food/order", food.order.createOrder);
+app.put("/food/order/:id", food.order.updateOrder);
+app.delete("/food/order/:id", food.order.deleteOrder);
+
+// food.store
+app.get("/food/store/:id", food.store.getStoreById);
+app.post("/food/store", food.store.createStore);
+app.put("/food/store/:id", food.store.updateStore);
+app.delete("/food/store/:id", food.store.deleteStore);
 
 // app.get("/*/:uid", async (req, res) => {});
 
